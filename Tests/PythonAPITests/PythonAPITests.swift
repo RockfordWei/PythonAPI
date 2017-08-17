587d71889500bf1fac98bcf99bbaa3012bc0326b
Rockford Wei <rocky@perfect.org>
2017-08-17 15:04:25 -0400

    Fixing the linker issue.

--- Tests/PythonAPITests/PythonAPITests.swift
+++ Tests/PythonAPITests/PythonAPITests.swift
@@ -1,14 +1,32 @@
 import XCTest
 @testable import PythonAPI
+import Foundation
 
 class PythonAPITests: XCTestCase {
-    func testExample() {
-      let p = PyObject()
-      print(p)
+
+  override func setUp() {
+    Py_Initialize()
+  }
+  func testExample() {
+    let p = PyObject()
+    print(p)
+  }
+
+  func testVersion() {
+    var versionString = strdup("version")
+    defer { free(versionString) }
+    if let module = PyImport_ImportModule("sys"),
+      let sys = PyModule_GetDict(module),
+      let verObj = PyMapping_GetItemString(sys, versionString),
+      let verstr = PyString_AsString(verObj),
+      let _ = strstr(verstr, "2.7") {
+      print(String(cString: verstr))
+    } else {
+      XCTFail("library import failed")
     }
-
-
-    static var allTests = [
-        ("testExample", testExample),
+  }
+  static var allTests = [
+    ("testExample", testExample),
+    ("testVersion", testVersion)
     ]
 }
