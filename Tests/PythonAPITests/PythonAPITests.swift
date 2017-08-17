7ce9a130b67668d271cbb6ffd7f95db15792f03b
Rockford Wei <rocky@perfect.org>
2017-08-17 16:25:39 -0400

    Adding test functions.

--- Tests/PythonAPITests/PythonAPITests.swift
+++ Tests/PythonAPITests/PythonAPITests.swift
@@ -1,32 +1,59 @@
 import XCTest
 @testable import PythonAPI
-import Foundation
 
 class PythonAPITests: XCTestCase {
 
   override func setUp() {
     Py_Initialize()
   }
+
+  override func tearDown() {
+    Py_Finalize()
+  }
+
   func testExample() {
     let p = PyObject()
     print(p)
   }
 
   func testVersion() {
-    var versionString = strdup("version")
-    defer { free(versionString) }
     if let module = PyImport_ImportModule("sys"),
       let sys = PyModule_GetDict(module),
-      let verObj = PyMapping_GetItemString(sys, versionString),
+      let verObj = PyMapping_GetItemString(sys, UnsafeMutablePointer<Int8>(mutating: "version")),
       let verstr = PyString_AsString(verObj),
       let _ = strstr(verstr, "2.7") {
       print(String(cString: verstr))
     } else {
-      XCTFail("library import failed")
+      XCTFail("version checking failed")
     }
   }
+
+  func testHello() {
+    let program = "def mydouble(num):\n\treturn num * 2;\n"
+    let path = "/tmp/helloworld.py"
+    let f = fopen(path, "w")
+    _ = program.withCString { pstr -> Int in
+      return fwrite(pstr, 1, program.characters.count, f)
+    }
+    fclose(f)
+    PySys_SetPath(UnsafeMutablePointer<Int8>(mutating: "/tmp"))
+    if let module = PyImport_ImportModule("helloworld"),
+      let function = PyObject_GetAttrString(module, "mydouble"),
+      let num = PyInt_FromLong(2),
+      let args = PyTuple_New(1),
+      PyTuple_SetItem(args, 0, num) == 0,
+      let res = PyObject_CallObject(function, args) {
+      let four = PyInt_AsLong(res)
+      XCTAssertEqual(four, 4)
+    } else {
+      XCTFail("library import failed")
+    }
+    unlink(path)
+  }
+
   static var allTests = [
     ("testExample", testExample),
-    ("testVersion", testVersion)
+    ("testVersion", testVersion),
+    ("testHello", testHello)
     ]
 }
